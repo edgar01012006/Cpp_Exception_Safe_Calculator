5d9b15aa8025c93d863cabee776cd62cd25c7d8c
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-11-30 12:28:47 +0400

    updated Calculator

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,40 +1,116 @@
 #include "Calculator.hpp"
 
+
+double Calculator::MaxDouble = std::numeric_limits<double>::max(); // 1.7976931348623157E308
+double Calculator::MinDouble = std::numeric_limits<double>::lowest(); // -1.7976931348623157E308
+
 void Calculator::setInputs() {
     std::cin >> input1;
     std::cin >> input2;
 }
 
+void Calculator::setInput() {
+    std::cin >> input1;
+}
+
 void Calculator::inputAdd() noexcept {
     setInputs();
     try {
-        double MaxDouble = std::numeric_limits<double>::max(); // 1.7976931348623157E308
-        double MinDouble = std::numeric_limits<double>::min(); // 2.2250738585072014E-308
         double num1 = std::stod(input1);
         double num2 = std::stod(input2);
-        if (MaxDouble - num1 < num2) {
+        if (MaxDouble - num1 < num2) { // num1 + num2 > MaxDouble
             throw std::out_of_range("out_of_range");
         }
-        if (MinDouble - num1 < num2) {
+        if (MinDouble - num1 > num2) { // num1 + num2 < MinDouble
             throw std::out_of_range("out_of_range");
         }
         std::cout << num1 << " + " << num2 << " = " << Calculator::add(num1, num2) << std::endl;
-    } catch(const std::invalid_argument& ex) {
-        std::cout << ex.what() << std::endl;
-    } catch(const std::out_of_range& ex) {
+    } catch(const std::logic_error& ex) {
         std::cout << ex.what() << std::endl;
     }
-    return;
 }
 
-void Calculator::inputSub() {
-
+void Calculator::inputSub() noexcept {
+    setInputs();
+    try {
+        double num1 = std::stod(input1);
+        double num2 = std::stod(input2);
+        if (MaxDouble + num2 < num1) { // num1 - num2 > MaxDouble
+            throw std::out_of_range("out_of_range");
+        }
+        if (MinDouble + num2 > num1) { // num1 - num2 < MinDouble
+            throw std::out_of_range("out_of_range");
+        }
+        std::cout << num1 << " - " << num2 << " = " << Calculator::sub(num1, num2) << std::endl;
+    } catch(const std::logic_error& ex) {
+        std::cout << ex.what() << std::endl;
+    }
 }
 
-/*
-void Calculator::inputMul();
-void Calculator::inputDiv();
-*/
+
+void Calculator::inputMul() noexcept {
+    setInputs();
+    try {
+        double num1 = std::stod(input1);
+        double num2 = std::stod(input2);
+        if (MaxDouble / num2 < num1) { // num1 * num2 > MaxDouble
+            throw std::out_of_range("out_of_range");
+        }
+        if (MinDouble / num2 > num1) { // num1 * num2 < MinDouble
+            throw std::out_of_range("out_of_range");
+        }
+        std::cout << num1 << " * " << num2 << " = " << Calculator::mul(num1, num2) << std::endl;
+    } catch(const std::logic_error& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
+
+
+void Calculator::inputDiv() noexcept {
+    setInputs();
+    try {
+        double num1 = std::stod(input1);
+        double num2 = std::stod(input2);
+        if (num2 == 0) {
+            throw std::invalid_argument("invalid_argument");
+        }
+        if (MaxDouble * num2 < num1) { // num1 / num2 > MaxDouble
+            throw std::out_of_range("out_of_range");
+        }
+        if (MinDouble * num2 > num1) { // num1 / num2 < MaxDouble
+            throw std::out_of_range("out_of_range");
+        }
+        std::cout << num1 << " / " << num2 << " = " << Calculator::div(num1, num2) << std::endl;
+    } catch(const std::logic_error& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
+
+void Calculator::inputSqrt() noexcept {
+    setInput();
+    try {
+        double num = std::stod(input1);
+        if (num < 0) {
+            throw std::invalid_argument("invalid_argument");
+        }
+        std::cout << "sqrt(" << num << ") = " << Calculator::sqrt(num) << std::endl;
+    } catch(const std::logic_error& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
+
+void Calculator::inputLog() noexcept {
+    setInput();
+    try {
+        double num = std::stod(input1);
+        if (num <= 0) {
+            throw std::invalid_argument("invalid_argument");
+        }
+        std::cout << "log" << num << " = " << Calculator::log(num) << std::endl;
+    } catch(const std::logic_error& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
 
 void Calculator::menu() {
     bool exit = true;
@@ -44,10 +120,12 @@ void Calculator::menu() {
                   << "1: add\n"
                   << "2: sub\n"
                   << "3: mul\n"
-                  << "4: div\n";
+                  << "4: div\n"
+                  << "5: sqrt\n"
+                  << "6: log\n";
         try {
             std::cin >> input;
-            if (input < 0 || input > 4) {
+            if (input < 0 || input > 6) {
                 throw std::invalid_argument("invalid_argument");
             }
             switch (input) {
@@ -55,12 +133,22 @@ void Calculator::menu() {
                     return;
                 case 1:
                     inputAdd();
+                    break;
                 case 2:
                     inputSub();
+                    break;
                 case 3:
-                    //inputMul();
+                    inputMul();
+                    break;
                 case 4:
-                    //inputDiv();
+                    inputDiv();
+                    break;
+                case 5:
+                    inputSqrt();
+                    break;
+                case 6:
+                    inputLog();
+                    break;
                 default:
                     break;
             } 
--- Calculator.hpp
+++ Calculator.hpp
@@ -2,24 +2,35 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <cmath>
+
 
 class Calculator {
     public:
-        static double add(double num1, double num2) { return num1 + num2; }
-        static double sub(double num1, double num2) { return num1 - num2; }
-        static double mul(double num1, double num2) { return num1 * num2; }
-        static double div(double num1, double num2) { return num1 / num2; }
+        static double add(double num1, double num2) noexcept { return num1 + num2; }
+        static double sub(double num1, double num2) noexcept { return num1 - num2; }
+        static double mul(double num1, double num2) noexcept { return num1 * num2; }
+        static double div(double num1, double num2) noexcept { return num1 / num2; }
+        static double sqrt(double num) noexcept { return std::sqrt(num); }
+        static double log(double num) noexcept { return std::log(num); }
 
         void inputAdd() noexcept;
         void inputSub() noexcept;
         void inputMul() noexcept;
         void inputDiv() noexcept;
+        void inputSqrt() noexcept;
+        void inputLog() noexcept;
 
         void menu();
 
         void setInputs();
+        void setInput();
 
     private:
         std::string input1;
         std::string input2;
+
+        static double MaxDouble;
+        static double MinDouble;
+
 };
